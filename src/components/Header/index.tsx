0858c06cfc9b7a8220cef2e8021cb0590fe9c897
Rafael Aragão <46876806+rafaelfaragao@users.noreply.github.com>
2021-08-20 01:00:33 -0300

    Create Components, add Miraje.JS and Axios, Create Modal Form

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
-import logoImg from '../../assets/logo.svg'
-import { Container, Content } from './styles'
+import logoImg from '../../assets/logo.svg';
 
-export function Header() {
+import { Container, Content } from './styles';
+
+interface PropsHeader {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: PropsHeader) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
       </Content>
